a3c9a64f7e07d7ed76195d4f53113ef5afb11401
Karlo Videk <karlo.videk@gmail.com>
2021-07-29 20:01:51 +0200

    admin test

--- content/home.md
+++ content/home.md
@@ -1,9 +1,4 @@
 ---
 title: Intuitive, minimalist and ahead of time.
 subtitle: We craft applicable UI/UX that reflects your business values. Functional, simple and human-centered products.
-featured_work:
-  - description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aperiam assumenda blanditiis debitis, dolore dolorum eos, et eum eveniet itaque magni numquam pariatur qui totam ut vel vitae! Laboriosam, tenetur.'
-    name: Digital.agency
-  - description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aperiam assumenda blanditiis debitis, dolore dolorum eos, et eum eveniet itaque magni numquam pariatur qui totam ut vel vitae! Laboriosam, tenetur.'
-    name: HLG.studio
 ---
--- next.config.js
+++ next.config.js
@@ -1,10 +1,48 @@
+// module.exports = {
+//     webpack: cfg => {
+//         cfg.module.rules.push({
+//             test: /\.md$/,
+//             loader: 'frontmatter-markdown-loader',
+//             options: { mode: ['react-component'] },
+//         });
+//         return cfg;
+//     },
+// };
+
+const fs = require('fs');
+const blogPostsFolder = './content/workPosts';
+
+const getPathsForPosts = () => {
+    return fs
+        .readdirSync(blogPostsFolder)
+        .map(blogName => {
+            const trimmedName = blogName.substring(0, blogName.length - 3);
+            return {
+                [`/work/${trimmedName}`]: {
+                    page: '/work/[slug]',
+                    query: {
+                        slug: trimmedName,
+                    },
+                },
+            };
+        })
+        .reduce((acc, curr) => {
+            return { ...acc, ...curr };
+        }, {});
+};
+
 module.exports = {
-    webpack: cfg => {
-        cfg.module.rules.push({
+    webpack: configuration => {
+        configuration.module.rules.push({
             test: /\.md$/,
-            loader: 'frontmatter-markdown-loader',
-            options: { mode: ['react-component'] },
+            use: 'frontmatter-markdown-loader',
         });
-        return cfg;
+        return configuration;
+    },
+    async exportPathMap(defaultPathMap) {
+        return {
+            ...defaultPathMap,
+            ...getPathsForPosts(),
+        };
     },
 };
