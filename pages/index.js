0bd22bfc7626a4259cc44443f0bcdefd9e1c0fde
Karlo Videk <karlo.videk@gmail.com>
2021-07-31 11:14:20 +0200

    home featured work WiP

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,31 @@
 import Head from 'next/head';
+import 'isomorphic-fetch';
+import Link from 'next/link';
+import Image from 'next/image';
 import { attributes } from '../content/home.md';
 
-const Home = () => {
+const importWorkPosts = async () => {
+    // https://medium.com/@shawnstern/importing-multiple-markdown-files-into-a-react-component-with-webpack-7548559fce6f
+    // second flag in require.context function is if subdirectories should be searched
+    const markdownFiles = require
+        .context('../content/workPosts', false, /\.md$/)
+        .keys()
+        .map(relativePath => relativePath.substring(2));
+
+    return Promise.all(
+        markdownFiles.map(async path => {
+            const markdown = await import(`../content/workPosts/${path}`);
+            return { ...markdown, slug: path.substring(0, path.length - 3) };
+        })
+    );
+};
+
+const Home = ({ postList }) => {
     let { title, subtitle, featured_work } = attributes;
+
     console.log(featured_work);
+
+    const filteredPostList = [];
 
     return (
         <>
@@ -25,6 +47,13 @@ const Home = () => {
             </div>
         </>
     );
+};
+
+home.getInitialProps = async () => {
+    console.log('getInitialProps');
+
+    const postList = await importWorkPosts();
+    return { postList };
 };
 
 export default Home;
