aee09e6ad449a6da8622d3684feaa78ef3705342
Karlo Videk <karlo.videk@gmail.com>
2021-08-06 13:15:56 +0200

    no idea

--- pages/work/[slug].js
+++ pages/work/[slug].js
@@ -3,9 +3,9 @@ import Image from 'next/image';
 import 'isomorphic-fetch';
 import importWorkPosts from '../../lib/importWorkPosts';
 
-const workPost = post => {
+const workPost = ({ post }) => {
     console.log('post: ', post);
-    const { title, subtitle, image } = post;
+    const { title, subtitle, image } = post.attributes;
 
     return (
         <div>
@@ -18,51 +18,51 @@ const workPost = post => {
     );
 };
 
-// workPost.getInitialProps = async ({ query }) => {
-//     const { slug } = query;
-//
-//     const post = await import(`../../content/workPosts/${slug}.md`).catch(error => null);
-//
-//     return { post };
-// };
+workPost.getInitialProps = async ({ query }) => {
+    const { slug } = query;
+
+    const post = await import(`../../content/workPosts/${slug}.md`).catch(error => null);
+
+    return { post };
+};
 
 export default workPost;
 
 // WTF this aint workin' right?
-export const getStaticProps = async context => {
-    console.log('context: ', context);
-    const { params } = context;
-
-    console.log('params: ', params);
-
-    const post = await import(`../../content/workPosts/${params.slug}.md`).catch(error =>
-        console.error(error)
-    );
-
-    console.log('getStaticProps post: ', post);
-
-    if (!post) {
-        return {
-            notFound: true,
-        };
-    }
-
-    return {
-        props: {
-            ...post,
-        },
-    };
-};
-
-export const getStaticPaths = async () => {
-    const postList = await importWorkPosts();
-    const paths = postList.map(post => ({ params: { slug: `/work/${post.slug}` } }));
-    // const paths = postList.map(({ slug }) => `/work/${slug}`);
-
-    console.log('paths: ', paths);
-
-    return {
-        paths,
-        fallback: true,
-    };
-};
+// export const getStaticProps = async context => {
+//     console.log('context: ', context);
+//     const { params } = context;
+//
+//     console.log('params: ', params);
+//
+//     const post = await import(`../../content/workPosts/${params.slug}.md`).catch(error =>
+//         console.error(error)
+//     );
+//
+//     console.log('getStaticProps post: ', post);
+//
+//     if (!post) {
+//         return {
+//             notFound: true,
+//         };
+//     }
+//
+//     return {
+//         props: {
+//             ...post,
+//         },
+//     };
+// };
+//
+// export const getStaticPaths = async () => {
+//     const postList = await importWorkPosts();
+//     const paths = postList.map(post => ({ params: { slug: `/work/${post.slug}` } }));
+//     // const paths = postList.map(({ slug }) => `/work/${slug}`);
+//
+//     console.log('paths: ', paths);
+//
+//     return {
+//         paths,
+//         fallback: true,
+//     };
+// };
